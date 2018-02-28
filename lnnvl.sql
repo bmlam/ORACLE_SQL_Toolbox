188e40933cbf75e97530214dec84e70ba078fb5d
Bon-Minh Lam <bmlam@sefrowo.com>
2018-02-28 15:01:31 +0100

    made demo better comprehensible

--- lnnvl.sql
+++ lnnvl.sql
@@ -17,6 +17,30 @@ select a, b
 from a_b
 ;
 
+-- The table has these rows:
+
+--SQL> select * from test_lnnvl order by a, b;
+--
+--         A          B
+------------ ----------
+--         1          1
+--         1          2
+--         1 null
+--         2          1
+--         2          2
+--         2 null
+--null                1
+--null                2
+--null       null
+--
+--9 rows selected.
+
+-- From an intuitive understanding of the use cases of LNNVL - the way I personally interpret
+-- purpose of this function - Logical Negation with special handling for Null VaLues, only the 1.
+-- the 5 and the last row should be excluded by the expression LNNVL( a = b ), i.e. I expect 6 rows
+-- to be returned.
+-- But see what the function really does:
+
 -- SQL> select * from test_lnnvl where lnnvl( a = b );
 -- 
 --          A          B
@@ -25,8 +49,12 @@ from a_b
 -- null                1
 -- null       null
 -- 
--- -- conclusion lnnvl is not suitable for eliminating redundant UPDATE in a MERGE statement. We would want following rows to be
--- -- eliminatie:
--- 
+-- It only returns 4 rows. The following rows are missing in the result set:
 --          1          1
 -- null       null
+
+-- One of the presumed use case for LNNVL is in a MERGE statement. It is suppose to update
+-- only those rows which differ in at least one column in the target and source result set
+-- taking null value into consideration. 
+-- I saw a few programmer uses this function because they LNNVL will take care of the null
+-- value nicely. But as we have seen, the function is not really up to this expectation.
