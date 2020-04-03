2847b9537b917e52e4071f458193929aa9ede9e6
bmlam <bmlam@sefrowo.com>
2020-04-03 22:39:13 +0200

    correct q length computation

--- view_queue_length_stats.sql
+++ view_queue_length_stats.sql
@@ -15,31 +15,36 @@ define p_days_back=&1
 define p_randomize_duration_mins=&2
 define p_group_by_date_mask=&3
 
-WITH data_ AS (
+WITH a_ AS (
 	SELECT object_id obj_id, 'queued' as STATUS
 		, created - dbms_random.value(low=> 0, high=> 9999 ) / 1440  as ENTER_QUEUE_TIME
-		, created + dbms_random.value(low=> 0, high=> &p_randomize_duration_mins ) / 1440 as EXIT_QUEUE_TIME
 	FROM dba_objects
 	WHERE created >= sysdate - &p_days_back
-), factory_ as (
+),  a_b_ AS (
+	SELECT obj_id, STATUS, ENTER_QUEUE_TIME
+		, ENTER_QUEUE_TIME + dbms_random.value(low=> 0, high=> &p_randomize_duration_mins ) / 1440 as EXIT_QUEUE_TIME
+	FROM a_
+), xxx_factory_ as (
 	SELECT level, (sysdate - &p_days_back) + level/1440 minit 
 	from dual
 	connect by level <=  &p_days_back * 1440
 ), flat_ AS (
-	SELECT status, f.minit, d.ENTER_QUEUE_TIME, d.EXIT_QUEUE_TIME, obj_id
-	FROM data_ d 
-	JOIN factory_ f ON f.minit >= d.ENTER_QUEUE_TIME and f.minit <= d.EXIT_QUEUE_TIME 
+	SELECT status, f.minit, obj_id, d.ENTER_QUEUE_TIME, d.EXIT_QUEUE_TIME
+		, to_char(f.minit, '&p_group_by_date_mask') group_key
+		, count( distinct obj_id) OVER ( PARTITION BY f.minit ) q_len_of_minit
+	FROM a_b_ d 
+	JOIN /*factory_ */ minits f ON f.minit >= d.ENTER_QUEUE_TIME and f.minit <= d.EXIT_QUEUE_TIME 
 ), add_group_key_ AS (
 	SELECT status
 		, obj_id, ENTER_QUEUE_TIME, EXIT_QUEUE_TIME
-		, to_char(minit, '&p_group_by_date_mask') group_key
 	FROM flat_
 )
-SELECT group_key time_slot, count(1) queue_len
+SELECT group_key time_slot, avg(q_len_of_minit) avg_q_len
 --select * 
-FROM add_group_key_
+FROM flat_ --  add_group_key_
 GROUP BY group_key
-ORDER BY group_key
+ORDER BY null -- minit, 
+, group_key
 ;
 
 -- test example: view_queue_length_stats.sql 1 30 rr.mm.dd_hh24
